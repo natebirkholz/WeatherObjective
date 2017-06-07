9721a233686bb6a305e439a5b6f845511d4f3ae2
natebirkholz <nbirkholz@gmail.com>
2017-06-07 12:45:01 -0700

    Initial Commit

---
+++ WeatherObjective/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherObjective
+//
+//  Created by Nathan Birkholz on 6/7/17.
+//  Copyright © 2017 natebirkholz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeatherObjective/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WeatherObjective
+//
+//  Created by Nathan Birkholz on 6/7/17.
+//  Copyright © 2017 natebirkholz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
